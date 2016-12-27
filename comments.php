99708ed82171542bd5b8357ac27a2753fd78cc83
Paul Kim <kimbaudi@gmail.com>
2016-12-27 08:57:23 -0800

    initial comment list, paging and form

---
+++ comments.php
@@ -0,0 +1,109 @@
+<?php if ( post_password_required() ) {
+    return;
+} ?>
+
+<div id="comments" class="comments-area">
+
+    <?php if ( have_comments() ) : ?>
+
+        <h2 class="comments-title title is-5">
+            <?php
+            $comments_number = get_comments_number();
+            if ( '1' === $comments_number ) {
+                /* translators: %s: post title */
+                /* 1 Response to &ldquo;%s&rdquo; */
+                printf(
+                    _x(
+                        '1 Response',
+                        'comments title',
+                        'bulmapress'
+                    ),
+                    get_the_title()
+                );
+            } else {
+                /* translators: 1: number of comments, 2: post title */
+                /* %1$s Response to &ldquo;%2$s&rdquo; */
+                /* %1$s Responses to &ldquo;%2$s&rdquo; */
+                printf(
+                    _nx(
+                        '%1$s Response',
+                        '%1$s Responses',
+                        $comments_number,
+                        'comments title',
+                        'bulmapress'
+                    ),
+                    number_format_i18n( $comments_number ),
+                    get_the_title()
+                );
+            }
+            ?>
+        </h2>
+
+<!--        <ol class="comment-list">-->
+<!--            --><?php
+//            wp_list_comments( array(
+//                'avatar_size' => 48,
+//                'style'       => 'ol',
+//                'short_ping'  => true,
+//                'reply_text'  => '<a class="button is-small"><span class="icon is-small"><i class="fa fa-reply"></i></span><span>'.__( 'Reply', 'bulmapress' ).'</span></a>',
+//                // 'reply_text'  => twentyseventeen_get_svg( array( 'icon' => 'mail-reply' ) ) . __( 'Reply', 'bulmapress' ),
+//            ) );
+//            ?>
+<!--        </ol>-->
+
+<!--        <ul class="comment-list">-->
+<!--            --><?php //wp_list_comments( 'type=comment&callback=mytheme_comment' ); ?>
+<!--        </ul>-->
+
+        <ol class="comment-list">
+            <?php
+            wp_list_comments( array(
+                'style'       => 'ol',
+                'callback' => 'bulmapress_comment',
+                'type' => 'comment',
+                'reply_text' => __( 'Reply', 'bulmapress' ),
+                //'reply_text'  => '<a class="button is-small"><span class="icon is-small"><i class="fa fa-reply"></i></span><span>'.__( 'Reply', 'bulmapress' ).'</span></a>',
+                //'page' => 1,
+                //'per_page' => 1,
+                'avatar_size' => 48,
+                'short_ping'  => false,
+            ) );
+            ?>
+        </ol>
+
+
+        <nav class="comment-nav nav">
+            <div class="nav-left">
+                <span class="nav-item">
+                <?php previous_comments_link(); ?>
+                </span>
+            </div>
+            <div class="nav-center">
+                <?php the_comments_pagination( array(
+                    'prev_text' => '<span>' . __( 'Previous', 'bulmapress' ) . '</span>',
+                    'next_text' => '<span>' . __( 'Next', 'bulmapress' ) . '</span>',
+                ) ); ?>
+            </div>
+            <div class="nav-right">
+                <span class="nav-item">
+                <?php next_comments_link(); ?>
+                </span>
+            </div>
+        </nav>
+
+
+        <?php
+
+    endif; // Check for have_comments().
+
+    // If comments are closed and there are comments, let's leave a little note, shall we?
+    if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+
+        <p class="no-comments"><?php _e( 'Comments are closed.', 'bulmapress' ); ?></p>
+    <?php
+    endif;
+
+    comment_form();
+    ?>
+
+</div><!-- #comments -->
--- functions.php
+++ functions.php
@@ -49,11 +49,11 @@ function twentyseventeen_setup() {
 	 * to output valid HTML5.
 	 */
 	add_theme_support( 'html5', array(
-		'comment-form',
 		'comment-list',
+        //'comment-form',
+        'search-form',
 		'gallery',
 		'caption',
-        'search-form'
 	) );
 
 	/*
@@ -428,6 +428,85 @@ add_filter( 'show_recent_comments_widget_style', '__return_false' );
 //// disable wpautop
 //remove_filter( 'the_content', 'wpautop' );
 //remove_filter( 'the_excerpt', 'wpautop' );
+
+
+function bulmapress_comment($comment, $args, $depth) {
+    if ( 'div' === $args['style'] ) {
+        $tag       = 'div';
+        $add_below = 'comment';
+    } else {
+        $tag       = 'li';
+        $add_below = 'div-comment';
+    }
+    ?>
+    <<?php echo $tag ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ) ?> id="comment-<?php comment_ID() ?>">
+    <article class="media" id="comment-<?php comment_ID() ?>">
+        <figure class="media-left">
+            <p class="image is-48x48">
+                <?php if ( $args['avatar_size'] != 0 ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+            </p>
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <strong>
+                    <?php printf( __( '%s' ), get_comment_author_link() ); ?>
+                </strong>
+                <small>
+                    <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>">
+                        <?php
+                        /* translators: 1: date, 2: time */
+                        printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time() );
+                        ?>
+                    </a>
+                </small>
+
+                <?php if ( $comment->comment_approved == '0' ) : ?>
+                    <p class="comment-awaiting-moderation">
+                        <?php _e( 'Your comment is awaiting moderation.' ); ?>
+                    </p>
+                <?php endif; ?>
+
+                <?php comment_text(); ?>
+                <small>
+                    <?php edit_comment_link( __( 'Edit' ), '  ', '' ); ?>
+                    <?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+                </small>
+            </div>
+        </div>
+    </article>
+
+<!--    <--><?php //echo $tag ?><!-- --><?php //comment_class( empty( $args['has_children'] ) ? '' : 'parent' ) ?><!-- id="comment---><?php //comment_ID() ?><!--">-->
+<!--    --><?php //if ( 'div' != $args['style'] ) : ?>
+<!--        <div id="div-comment---><?php //comment_ID() ?><!--" class="comment-body">-->
+<!--    --><?php //endif; ?>
+<!--    <div class="comment-author vcard">-->
+<!--        --><?php //if ( $args['avatar_size'] != 0 ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+<!--        --><?php //printf( __( '<cite class="fn">%s</cite> <span class="says">says:</span>' ), get_comment_author_link() ); ?>
+<!--    </div>-->
+<!--    --><?php //if ( $comment->comment_approved == '0' ) : ?>
+<!--        <em class="comment-awaiting-moderation">--><?php //_e( 'Your comment is awaiting moderation.' ); ?><!--</em>-->
+<!--        <br />-->
+<!--    --><?php //endif; ?>
+<!---->
+<!--    <div class="comment-meta commentmetadata"><a href="--><?php //echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?><!--">-->
+<!--            --><?php
+//            /* translators: 1: date, 2: time */
+//            printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time() ); ?><!--</a>--><?php //edit_comment_link( __( '(Edit)' ), '  ', '' );
+//        ?>
+<!--    </div>-->
+<!---->
+<!--    --><?php //comment_text(); ?>
+<!---->
+<!--    <div class="reply">-->
+<!--        --><?php //comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+<!--    </div>-->
+<!--    --><?php //if ( 'div' != $args['style'] ) : ?>
+<!--        </div>-->
+<!--    --><?php //endif; ?>
+
+    <?php
+}
+
 
 remove_action('wp_head', 'wp_generator');
 
