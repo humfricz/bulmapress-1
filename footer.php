4d1279949190d33ef64784e42003045839e81d82
Paul Kim <kimbaudi@gmail.com>
2016-12-28 02:43:55 -0800

    update footer link and css

--- footer.php
+++ footer.php
@@ -38,25 +38,25 @@
 
 		<nav class="level is-mobile">
 			<div class="level-item has-text-centered">
-				<a href="/" class="icon" aria-label="RSS"><i class="fa fa-rss" aria-hidden="true"></i></a>
+				<a href="https://blog.paul.kim/feed" class="icon" aria-label="RSS"><i class="fa fa-rss" aria-hidden="true"></i></a>
 			</div>
 			<div class="level-item has-text-centered">
-				<a href="/" class="icon" aria-label="GitHub"><i class="fa fa-github" aria-hidden="true"></i></a>
+				<a href="https://github.com/kimbaudi" class="icon" aria-label="GitHub"><i class="fa fa-github" aria-hidden="true"></i></a>
 			</div>
 			<div class="level-item has-text-centered">
-				<a href="/" class="icon" aria-label="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+				<a href="https://twitter.com/kimbaudi" class="icon" aria-label="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
 			</div>
 			<div class="level-item has-text-centered">
-				<a href="/" class="icon" aria-label="CodePen"><i class="fa fa-codepen" aria-hidden="true"></i></a>
+				<a href="http://codepen.io/kimbaudi" class="icon" aria-label="CodePen"><i class="fa fa-codepen" aria-hidden="true"></i></a>
 			</div>
 			<div class="level-item has-text-centered">
-				<a href="/" class="icon" aria-label="StackOverflow"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a>
+				<a href="http://stackoverflow.com/users/1097123/kimbaudi" class="icon" aria-label="StackOverflow"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a>
 			</div>
 			<div class="level-item has-text-centered">
-				<a href="/" class="icon" aria-label="Google+"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+				<a href="https://plus.google.com/u/0/118080331960200612622" class="icon" aria-label="Google+"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
 			</div>
 			<div class="level-item has-text-centered">
-				<a href="/" class="icon" aria-label="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				<a href="https://www.facebook.com/profile.php?id=100013239162410" class="icon" aria-label="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
 			</div>
 			<div class="level-item has-text-centered">
 				<a href="/" class="icon" aria-label="LinkedIn"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
